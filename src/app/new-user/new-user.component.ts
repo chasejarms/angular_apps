5840d2fe35f7aa3a0832bc70a2d542011bf3d36b
Chase Armstrong <chasejarms@gmail.com>
2017-05-24 13:34:17 -0600

    Practice two-way data binding

---
+++ src/app/new-user/new-user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.css']
+})
+export class NewUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  username: string = '';
+
+  noUsername() {
+    return this.username === ''
+  };
+
+  resetUsername() {
+    this.username = '';
+  };
+
+}
